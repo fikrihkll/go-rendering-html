e219069e6a5df12dffbfd3a1132d366b98b6ab40
Fikri Haikal <fikri.haikal@pinhome.id>
2025-05-23 12:52:12 +0700

    refactor: change deeplink

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func handleInterstitial(w http.ResponseWriter, r *http.Request) {
 	}
 
 	data := TemplateData{
-		DeepLink:        "https://staging-consumer.pinhome.id/app/test",
+		DeepLink:        "https://staging-dynamic-link.pinhome.dev/pinhome-staging/app/project-detail?buildingType=building_type.house&digital_sharing=seeker_link&listingType=property_listing_type.sell&marketType=property_market_type.primary&slug=dqa-rumah-primary-residential&utm_campaign=brokerage_seeker_conversion_engaged_digital_sharing_seeker&utm_medium=Referral&utm_source=consumer_app",
 		FallbackURL:     "https://staging.pinhome.id",
 		AndroidStoreUrl: "https://play.google.com/store/apps/details?id=id.pinhome.consumer",
 		IosStoreUrl:     "https://apps.apple.com/id/app/pinhome-properti-kpr-jasa/id1558641251",

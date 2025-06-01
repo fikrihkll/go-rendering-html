11642bc6a0bd1006eb038617b6a7d7150dad2779
Fikri Haikal <fikri.haikal@pinhome.id>
2025-06-01 19:31:45 +0700

    feat: add chain

--- main.go
+++ main.go
@@ -32,9 +32,9 @@ func handleInterstitial(w http.ResponseWriter, r *http.Request) {
 	// fallbackURL := "https://staging.pinhome.id"
 	// actualLink := "https://consumer.pinhome.id/app/owom"
 
-	shortLink := "https://link.fikrihkl.me/consumer-staging/test"
+	shortLink := "https://preview-link.fikrihkl.me/consumer-staging/test"
 	fallbackURL := "https://staging.pinhome.id"
-	actualLink := "https://link.fikrihkl.me/consumer-staging/test"
+	actualLink := "https://preview-link.fikrihkl.me/consumer-staging/test"
 
 	autoLaunched := r.URL.Query().Get("autoLaunch")
 	buttonPressed := r.URL.Query().Get("buttonPressed")

c11f5ed2a0c588069364cbc91750453b32a75d1f
Fikri Haikal <fikri.haikal@pinhome.id>
2025-06-25 22:31:49 +0700

    refactor: change redirect link to fdl

--- preview/main.go
+++ preview/main.go
@@ -32,7 +32,7 @@ func handlePreview(w http.ResponseWriter, r *http.Request) {
 	// fmt.Sprintf("https://%s/%s/%s?buttonPressed=true", host, slug, shortkey)
 
 	data := PreviewData{
-		RedirectLink: "https://staging-dynamic-link.pinhome.dev/consumer-staging/cYst1JQhk",
+		RedirectLink: "https://staging-consumer.pinhome.id/app/?link=https://staging-consumer.pinhome.id/app/phs-web-view?url=https%3A%2F%2Fstaging-hs.pinhome.id%2Fmultipurpose%2Fd3077880-35f5-11f0-9673-42010a6bf0dc&apn=id.pinhome.consumer.staging&ibi=id.pinhome.consumer.staging&isi=1556796944",
 		ButtonText:   "Open in App",
 	}
 
